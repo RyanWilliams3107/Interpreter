b13da938d1ba83dd665dcc5c075e2047ad879c12
Ryan Williams <ryanwilliams_17@hotmail.co.uk>
2021-03-04 13:17:02 +0000

    s

--- Interpreter/Source.cpp
+++ Interpreter/Source.cpp
@@ -9,24 +9,28 @@ int main()
 	
 
 	std::string statement;
-	std::cout << ">>>";
-	std::getline(std::cin, statement);
-	std::cout << std::endl;
-	std::cout << statement << std::endl;
-
-	Lexer l("<stdin>", statement);
-	std::pair<std::vector<std::variant<IntegerToken, FloatToken, StringToken, OperatorToken>>, bool> generatedTokens = l.GenerateTokens();
-	std::vector<std::variant<IntegerToken, FloatToken, StringToken, OperatorToken>> tokens = generatedTokens.first;
-	bool error = generatedTokens.second;
-
-	for (auto& v : tokens)
+	while (true)
 	{
-		
-		std::visit([](auto&& arg) { 
-			std::cout << arg.GetTokenType() << ": " << arg.GetTokenValue() <<  " "; 
-			}, v);
-		
+		std::cout << ">>>";
+		std::getline(std::cin, statement);
+		std::cout << std::endl;
+		std::cout << statement << std::endl;
 
+		Lexer l("<stdin>", statement);
+		std::pair<std::vector<std::variant<IntegerToken, FloatToken, StringToken, OperatorToken>>, bool> generatedTokens = l.GenerateTokens();
+		std::vector<std::variant<IntegerToken, FloatToken, StringToken, OperatorToken>> tokens = generatedTokens.first;
+		bool error = generatedTokens.second;
+
+		for (auto& v : tokens)
+		{
+
+			std::visit([](auto&& arg) {
+				std::cout << arg.GetTokenType() << ": " << arg.GetTokenValue() << " ";
+				}, v);
+
+
+		}
+		std::cout << std::endl;
 	}
 	std::cin.get();
 	return 0;
