67982d80a136a6fc972d9ad6b1cfea50a9be6fdd
Ryan Williams <ryanwilliams_17@hotmail.co.uk>
2021-03-05 21:06:48 +0000

    s

--- Interpreter/src/InsertArrowsIntoString.cpp
+++ Interpreter/src/InsertArrowsIntoString.cpp
@@ -2,17 +2,35 @@
 
 std::string InsertArrowsIntoString(const std::string& Text, Position Start, Position End)
 {
+    std::string Result = "";
     int IndexStart = std::max((int)Text.rfind("\n", 0, Start.GetIndex()), 0);
     int IndexEnd = Text.find('\n', IndexStart + 1);
     if (IndexEnd < 0)
     {
         IndexEnd = (int)Text.length();
     }
-
     int LineCount = End.GetLineNumber() - Start.GetLineNumber() + 1;
     for (int i = 0; i < LineCount; i++)
     {
         std::string Line = Text.substr(IndexStart, IndexEnd);
+        int ColumnStart = 0;
+        if (i == 0)
+        {
+            ColumnStart = Start.GetColumnNumber();
+        }
+        int ColumnEnd = Line.length() - 1;
+        if (i == LineCount)
+        {
+            ColumnEnd = End.GetColumnNumber();
+        }
+        Result += Line + "\n";
+        Result += ' ' * ColumnEnd + '^' * (ColumnEnd - ColumnStart);
+        IndexStart = IndexEnd;
+        IndexEnd = Text.find("\n", IndexStart + 1);
+        if (IndexEnd < 0)
+        {
+            IndexEnd = Text.length();
+        }
     }
-    return std::string(0);
+    return Result;
 }
